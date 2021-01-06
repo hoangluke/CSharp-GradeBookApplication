210ed44d120add52d56608b65b3135c82e1fb9d6
Luke Hoang <hoangl@protonmail.com>
2021-01-06 21:50:36 +1000

    Redefine letter grades for ranked grade books

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using System.Linq;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,49 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var orderedStudents = Students.OrderByDescending(student => student.AverageGrade);
+
+            int rank = 1;
+            foreach (var student in orderedStudents)
+            {
+                if (averageGrade >= student.AverageGrade)
+                {
+                    break;
+                }
+                rank += 1;
+            }
+
+            double percentage = ((double)rank / Students.Count) * 100;
+
+            if (percentage <= 20)
+            {
+                return 'A';
+            }
+            else if (percentage <= 40)
+            {
+                return 'B';
+            }
+            else if (percentage <= 60)
+            {
+                return 'C';
+            }
+            else if (percentage <= 80)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }
